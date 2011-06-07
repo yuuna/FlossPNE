587234f6fa4238ddcbc584c5818a9c3b136a9458
tozuka <tozuka@tejimaya.com>
2011-06-07 21:33:22 +0900

    added modules (ashiato, community, diary, favorite, message) directly under apps/smartphone_frontend; although these should be placed under each plugins

---
+++ apps/smartphone_frontend/modules/diary/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * diary actions.
+ *
+ * @package    OpenPNE
+ * @subpackage diary
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ *
+ * this should be placed under plugins/opDiaryPlugin/apps/smartphone_frontend/modules/diary/actions/
+ */
+class diaryActions extends opDiaryPluginDiaryActions
+{
+}
---
+++ apps/smartphone_frontend/modules/message/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * message actions.
+ *
+ * @package    OpenPNE
+ * @subpackage message
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class messageActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
