f743432d85c020a8d5a57aa103e586c5a868dc20
tozuka <tozuka@tejimaya.com>
2011-06-08 15:10:57 +0900

    日記のコメントを表示

--- apps/smartphone_frontend/modules/diary/templates/showSuccess.php
+++ apps/smartphone_frontend/modules/diary/templates/showSuccess.php
@@ -18,5 +18,23 @@
   </p>
   <?php endif; ?>
 
-     <p><?php echo auto_link_text(op_decoration(nl2br($diary->body))) ?></p>
+  <p><?php echo auto_link_text(op_decoration(nl2br($diary->body))) ?></p>
+  <hr/>
+    
+  <?php $commentCount = $diary->countDiaryComments() ?>
+  <div data-role="collapsible" data-collapsed="true">
+    <h3>コメント (<?php echo $commentCount ?>件)</h3>
+
+    <?php $pager = Doctrine::getTable('DiaryComment')->getDiaryCommentPagerForDiary($diary->getId(), $request['page'], 20); ?>
+    <ul data-role="listview">
+    <?php foreach ($pager->getResults() as $diaryComment): ?>
+    <li><p class="ui-li-aside"><?php echo $diaryComment->getCreatedAt() ?></p>
+        <?php echo op_image_tag_sf_image($diaryComment->Member->getImageFileName(), array('size' => '76x76')) ?>
+        <h4><?php echo $diaryComment->Member->getName() ?>:</h4>
+        <p><?php echo $diaryComment->getBody() ?></p>
+    </li>
+    <?php endforeach ?>
+    </ul>
+  </div>
 </div>
+
