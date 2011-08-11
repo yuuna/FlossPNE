fbb6d9acb33d20962923f6dc1754cd5d2c22d2b4
Tajima Itsuro <niryuu@gmail.com>
2011-08-11 09:28:49 +0900

    fixed member activity

--- apps/smartphone_frontend/modules/member/templates/showActivitySuccess.php
+++ apps/smartphone_frontend/modules/member/templates/showActivitySuccess.php
@@ -5,9 +5,9 @@
 <?php $uri = $activityData->getUri(); ?>
   <li>
     <?php if ($uri) echo '<a href="'.url_for($uri).'">' ?>
-    <!-- <?php echo op_image_tag_sf_image($activityData->Member->getImageFileName(), array('size' => '76x76')) ?> -->
+    <?php echo op_image_tag_sf_image($activityData->Member->getImageFileName(), array('size' => '76x76')) ?>
     <p class="ui-li-aside"><?php echo substr($activityData->getCreatedAt(),11) ?></p>
-    <!-- <h4><?php echo $activityData->Member->getName() ?>:</h4> -->
+    <h4><?php echo $activityData->Member->getName() ?>:</h4>
     <p><?php echo __($activityData->getBody()) ?></p>
     <?php if ($uri) echo '</a>' ?>
   </li>
