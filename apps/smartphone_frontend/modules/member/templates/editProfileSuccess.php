877ec5384a5ef847066807eed18233bb5d2708d4
Tajima Itsuro <niryuu@gmail.com>
2011-08-11 09:05:51 +0900

    added member profile edit

---
+++ apps/smartphone_frontend/modules/member/templates/editProfileSuccess.php
@@ -0,0 +1,8 @@
+<?php slot('title', __('Edit Profile')) ?>
+<?php
+$options = array(
+  'title' => __('Edit Profile'),
+  'url' => url_for('@member_editProfile'),
+);
+op_include_form('profileForm', array($memberForm, $profileForm), $options)
+?>
